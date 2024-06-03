4fd1da2d263f460c29064f8c86ca9043432dc263
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-03 15:30:36 +0300

    feat: expand authRoutes (part.2) - add logout

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import { validateBody } from '#middlewares/index.js';
+import { authenticate, validateBody } from '#middlewares/index.js';
 import { joiSchemas } from '#validators/userValidator.js';
 import { authController } from '#controllers/index.js';
 
 const router = Router();
 
 router.post('/login', validateBody(joiSchemas.login), authController.login);
+
+router.get('/logout', authenticate, authController.logout);
 
 export { router as authRoutes };
