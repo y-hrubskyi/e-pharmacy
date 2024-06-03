91ef6b80ef15c0e4ced63f595f3ed37f19e22c6c
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-03 13:09:03 +0300

    feat: upd userRoutes (part.1) - add login

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import { validateBody } from '#middlewares/index.js';
+import { joiSchemas } from '#validators/userValidator.js';
+import { userController } from '#controllers/index.js';
+
 const router = Router();
+
+router.post('/login', validateBody(joiSchemas.login), userController.login);
 
 export { router as userRoutes };
