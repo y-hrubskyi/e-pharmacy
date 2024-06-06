e10f63b53ae4f9083bb6e7da5abb0f8d62f0e986
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-06 15:52:03 +0300

    feat: upd supplierRoutes (part.2) - add create

--- backend/src/routes/supplierRoutes.js
+++ backend/src/routes/supplierRoutes.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 
-import { authenticate } from '#middlewares/index.js';
+import { authenticate, validateBody } from '#middlewares/index.js';
+import { joiSchemas } from '#validators/supplierValidator.js';
 import { supplierController } from '#controllers/index.js';
 
 const router = Router();
 
 router.get('/', authenticate, supplierController.getAll);
+
+router.post(
+  '/',
+  authenticate,
+  validateBody(joiSchemas.create),
+  supplierController.create
+);
 
 export { router as supplierRoutes };
