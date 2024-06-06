bf682f07e96b3ca9d9befebdafda98edc48bc569
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-06 15:43:36 +0300

    feat: upd supplierRoutes (part.1) - add getAll

--- backend/src/routes/supplierRoutes.js
+++ backend/src/routes/supplierRoutes.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
 
+import { authenticate } from '#middlewares/index.js';
+import { supplierController } from '#controllers/index.js';
+
 const router = Router();
+
+router.get('/', authenticate, supplierController.getAll);
 
 export { router as supplierRoutes };
