207f36d854bd63cfff4921ae2354e67267873eb2
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-06 17:00:46 +0300

    chore: add empty line for better code readability

--- backend/src/controllers/suppliers/getAll.js
+++ backend/src/controllers/suppliers/getAll.js
@@ -3,6 +3,7 @@ import { supplierService } from '#services/index.js';
 export const getAll = async (req, res, next) => {
   try {
     const { page = 1, limit = 5, name } = req.query;
+
     const result = await supplierService.getAll(page, limit, name);
     res.json(result);
   } catch (error) {
