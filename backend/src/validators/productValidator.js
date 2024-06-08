e0557130e4eb11460d1b7575f05bca5ed15b3ec4
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-08 12:04:28 +0300

    feat: create product validation schema

--- backend/src/validators/productValidator.js
+++ backend/src/validators/productValidator.js
@@ -1,3 +1,5 @@
+import Joi from 'joi';
+
 export const Regexps = {
   NAME: /^[a-zA-Z0-9_\- ]{3,30}$/,
   SUPPLIER: /^[a-zA-Z0-9_\- ]{3,30}$/,
@@ -18,4 +20,21 @@ const categories = [
 
 export const Enums = {
   CATEGORIES: categories,
+};
+
+const productSchema = Joi.object({
+  name: Joi.string().pattern(Regexps.NAME).required(),
+  category: Joi.string()
+    .equal(...Enums.CATEGORIES)
+    .required(),
+  stock: Joi.number().min(0).required(),
+  supplier: Joi.object({
+    id: Joi.string().pattern(Regexps.MONGOOSE_OBJECT_ID).required(),
+    name: Joi.string().pattern(Regexps.SUPPLIER).required(),
+  }),
+  price: Joi.number().min(0.01).required(),
+});
+
+export const joiSchemas = {
+  create: productSchema,
 };
