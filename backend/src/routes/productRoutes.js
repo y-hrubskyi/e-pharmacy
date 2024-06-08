21c20da3d6df0c73581813cf7f3b1507fecfb6c9
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-08 12:05:07 +0300

    feat: upd productRoutes (part.2) - add create

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 
-import { authenticate } from '#middlewares/index.js';
+import { authenticate, validateBody } from '#middlewares/index.js';
+import { joiSchemas } from '#validators/productValidator.js';
 import { productController } from '#controllers/index.js';
 
 const router = Router();
 
 router.get('/', authenticate, productController.getAll);
+
+router.post(
+  '/',
+  authenticate,
+  validateBody(joiSchemas.create),
+  productController.create
+);
 
 export { router as productRoutes };
