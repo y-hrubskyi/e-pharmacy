060610e3810f71a830df6bc887c7645ddcac02e7
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-08 11:53:13 +0300

    feat: upd productRoutes (part.1) - add getAll

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
 
+import { authenticate } from '#middlewares/index.js';
+import { productController } from '#controllers/index.js';
+
 const router = Router();
+
+router.get('/', authenticate, productController.getAll);
 
 export { router as productRoutes };
