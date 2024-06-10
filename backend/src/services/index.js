edb7ee56356a4e6b17bfc4f1cd3d5048ec912921
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-10 15:28:09 +0300

    feat: create customerService (part.1) - get
     - create method of service - get (with query params and pagination)

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,3 +1,4 @@
 export * as authService from './auth/index.js';
 export * as supplierService from './suppliers/index.js';
 export * as productService from './products/index.js';
+export * as customerService from './customers/index.js';
