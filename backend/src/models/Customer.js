f921dfa8eb48160b84b83d3ef0f769d71504f62f
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-10 15:26:31 +0300

    feat: create Customer mongoose model

---
+++ backend/src/models/Customer.js
@@ -0,0 +1,54 @@
+import { Schema, model } from 'mongoose';
+import { isValid, parse } from 'date-fns';
+
+import { Regexps } from '#validators/customerValidator.js';
+
+const dateValidator = [
+  function (value) {
+    const date = parse(value, 'MMM d, yyyy', new Date());
+    return isValid(date);
+  },
+  'Invalid date format',
+];
+
+const customerSchema = new Schema(
+  {
+    image: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      match: Regexps.NAME,
+      required: [true, 'Customer info is required'],
+    },
+    email: {
+      type: String,
+      match: Regexps.EMAIL,
+      required: [true, 'Email is required'],
+    },
+    spent: {
+      type: Number,
+      min: 0,
+      required: [true, 'Spent is required'],
+    },
+    phone: {
+      type: String,
+      match: Regexps.PHONE,
+      required: [true, 'Phone is required'],
+    },
+    address: {
+      type: String,
+      match: Regexps.ADDRESS,
+      required: [true, 'Address is required'],
+    },
+    registerDate: {
+      type: String,
+      validate: dateValidator,
+      required: [true, 'Register date is required'],
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+export const Customer = model('Customer', customerSchema);
---
+++ backend/src/validators/customerValidator.js
@@ -0,0 +1,6 @@
+export const Regexps = {
+  NAME: /^[a-zA-Z0-9_\- ]{3,30}$/,
+  EMAIL: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  PHONE: /^\+380\d{9}$/,
+  ADDRESS: /^[a-zA-Z0-9\s,'-]+$/,
+};
