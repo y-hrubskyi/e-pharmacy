f8b786023313f0dbd48ac9c011568c1010066988
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-10 15:28:51 +0300

    feat: create customerController (part.1) - get
     - create method of controller - get (with query params and pagination)

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,3 +1,4 @@
 export * as authController from './auth/index.js';
 export * as supplierController from './suppliers/index.js';
 export * as productController from './products/index.js';
+export * as customerController from './customers/index.js';
