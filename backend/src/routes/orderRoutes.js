a58b54daf42f97854e11d1f611a9324af96585e4
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-11 17:02:35 +0300

    feat: upd orderRoutes (part.1) - add get

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
 
+import { authenticate } from '#middlewares/index.js';
+import { orderController } from '#controllers/index.js';
+
 const router = Router();
+
+router.get('/', authenticate, orderController.get);
 
 export { router as orderRoutes };
