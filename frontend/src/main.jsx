89acec7866d49fe9b0a2dd7a7f8303998af14559
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-16 21:48:20 +0300

    feat: configure styles (part.2)
     - create theme-constant
     - add ThemeProvider for easy of styling (using created theme-constant)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
+import { theme } from "#styles/theme.js";
+import { GlobalStyle } from "#styles/GlobalStyle.js";
 import { App } from "./App.jsx";
-import { GlobalStyle } from "./styles/GlobalStyle.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <GlobalStyle />
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
