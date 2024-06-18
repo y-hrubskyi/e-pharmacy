6544ca3d7a3a1225fbd08f3f2a0ebb9459d8e735
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-18 15:48:26 +0300

    feat: create reusable PasswordField component
    - with validation messages also - where children will be an input element (BaseInput as a prototype)

---
+++ frontend/src/components/common/PasswordField/PasswordField.jsx
@@ -0,0 +1,35 @@
+import { Icons } from "#config/icons";
+
+import {
+  CorrectMessage,
+  ErrorMessage,
+} from "#components/common/TextField/TextField.styled";
+import * as SC from "./PasswordField.styled";
+
+export const PasswordField = ({
+  isCorrect,
+  correctMessage,
+  hasError,
+  errorMessage,
+  passwordShown,
+  onTogglePasswordShown,
+  children,
+}) => {
+  return (
+    <SC.FieldWrapper>
+      {children}
+      <SC.EyeBtn
+        type="button"
+        onClick={onTogglePasswordShown}
+        aria-label={`${passwordShown ? "hide" : "show"} password`}
+        data-was-valid={isCorrect || hasError}
+      >
+        <SC.EyeIcon>
+          <use href={passwordShown ? Icons.eyeOn : Icons.eyeOff}></use>
+        </SC.EyeIcon>
+      </SC.EyeBtn>
+      {isCorrect && <CorrectMessage>{correctMessage}</CorrectMessage>}
+      {hasError && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </SC.FieldWrapper>
+  );
+};
