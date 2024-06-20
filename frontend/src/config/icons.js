d310353125d6ed4dc32568685830298d36554c0b
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-20 07:16:25 +0300

    feat: prepare to creating Header
     - add logo img
     - add logout and mobile menu icons to sprite
     - upd icons config (logout, mobileMenu)

--- frontend/src/config/icons.js
+++ frontend/src/config/icons.js
@@ -1,4 +1,9 @@
 export const Icons = {
   eyeOn: new URL("#assets/icons/sprite.svg#icon-eye-on", import.meta.url),
   eyeOff: new URL("#assets/icons/sprite.svg#icon-eye-off", import.meta.url),
+  logout: new URL("#assets/icons/sprite.svg#icon-logout", import.meta.url),
+  mobileMenu: new URL(
+    "#assets/icons/sprite.svg#icon-mobile-menu",
+    import.meta.url
+  ),
 };
