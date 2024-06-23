117a374f232932eef1e56b9db07abd32bd402f77
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-23 16:37:34 +0300

    feat: add data fetching states for DashboardPage

--- frontend/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage/DashboardPage.jsx
@@ -5,26 +5,40 @@ import API from "#services/axios";
 import { Statistics } from "#components/Statistics/Statistics";
 import { RecentCustomersTable } from "#components/RecentCustomersTable/RecentCustomersTable";
 import { IncomesExpensesTable } from "#components/IncomesExpensesTable/IncomesExpensesTable";
+import { Loader } from "#components/common/Loader/Loader";
+import { Placeholder } from "#components/common/Placeholder/Placeholder";
 
 import * as SC from "./DashboardPage.styled";
 
 const DashboardPage = () => {
   const [dashboardInfo, setDashboardInfo] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     (async () => {
       try {
+        setIsLoading(true);
+        setError(null);
+
         const { data } = await API.get("/dashboard");
         setDashboardInfo(data);
       } catch (error) {
-        //
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     })();
   }, []);
 
+  const loading = !error && isLoading;
+  const hasError = !isLoading && error;
+  const content = !isLoading && !error && dashboardInfo;
+  const noData = !isLoading && !error && !dashboardInfo;
+
   return (
     <SC.PageWrapper>
-      {dashboardInfo && (
+      {content && (
         <>
           <Statistics
             productsQuantity={dashboardInfo.productsQuantity}
@@ -42,6 +56,17 @@ const DashboardPage = () => {
             </SC.TableWrapper>
           </SC.TablesContainer>
         </>
+      )}
+      {loading && <Loader />}
+      {hasError && (
+        <SC.PlaceholderWrapper>
+          <Placeholder>Oops... {error}</Placeholder>
+        </SC.PlaceholderWrapper>
+      )}
+      {noData && (
+        <SC.PlaceholderWrapper>
+          <Placeholder>Nothing found</Placeholder>
+        </SC.PlaceholderWrapper>
       )}
     </SC.PageWrapper>
   );
--- frontend/src/pages/DashboardPage/DashboardPage.styled.js
+++ frontend/src/pages/DashboardPage/DashboardPage.styled.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { TableWrapper as BaseTableWrapper } from "#components/common/Table/Table.styled";
 
 export const PageWrapper = styled.div`
+  height: 100%;
+
   padding-top: ${(p) => p.theme.spacing(5)};
   padding-bottom: ${(p) => p.theme.spacing(20)};
 
@@ -32,4 +34,12 @@ export const TableWrapper = styled(BaseTableWrapper)`
   @media screen and (min-width: 1440px) {
     flex-basis: calc((100% - 20px) / 2);
   }
+`;
+
+export const PlaceholderWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
