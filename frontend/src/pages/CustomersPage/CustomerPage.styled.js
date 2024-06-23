564e8e05ef88a6e4e20c412e6730e0be877a2ea7
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-23 17:06:39 +0300

    feat: add to CustomersPage created earlier components

---
+++ frontend/src/pages/CustomersPage/CustomerPage.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+import { TableWrapper as BaseTableWrapper } from "#components/common/Table/Table.styled";
+
+export const TableWrapper = styled(BaseTableWrapper)`
+  padding-bottom: 20px;
+`;
--- frontend/src/pages/CustomersPage/CustomersPage.jsx
+++ frontend/src/pages/CustomersPage/CustomersPage.jsx
@@ -1,5 +1,21 @@
+import { PageWrapper, ControlPanel } from "#components/common/Page/Page.styled";
+import { Filter } from "#components/common/Filter/Filter";
+import { CustomersDataTable } from "#components/CustomersDataTable/CustomersDataTable";
+
+import * as SC from "./CustomerPage.styled";
+
 const CustomersPage = () => {
-  return <div>CustomersPage</div>;
+  return (
+    <PageWrapper>
+      <ControlPanel>
+        <Filter placeholder="User Name" fieldName="User Name" />
+      </ControlPanel>
+
+      <SC.TableWrapper>
+        <CustomersDataTable customers={[]} />
+      </SC.TableWrapper>
+    </PageWrapper>
+  );
 };
 
 export default CustomersPage;
