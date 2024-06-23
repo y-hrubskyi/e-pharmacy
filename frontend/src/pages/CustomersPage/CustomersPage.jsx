a4083e4ad9484fe8866eaebc51cabc79b040316e
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-23 17:08:32 +0300

    feat: add simple fetching customers data

--- frontend/src/pages/CustomersPage/CustomersPage.jsx
+++ frontend/src/pages/CustomersPage/CustomersPage.jsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+
+import API from "#services/axios";
+
 import { PageWrapper, ControlPanel } from "#components/common/Page/Page.styled";
 import { Filter } from "#components/common/Filter/Filter";
 import { CustomersDataTable } from "#components/CustomersDataTable/CustomersDataTable";
@@ -5,15 +9,30 @@ import { CustomersDataTable } from "#components/CustomersDataTable/CustomersData
 import * as SC from "./CustomerPage.styled";
 
 const CustomersPage = () => {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await API.get("/customers");
+        setCustomers(data.paginatedResult);
+      } catch (error) {
+        //
+      }
+    })();
+  }, []);
+
   return (
     <PageWrapper>
       <ControlPanel>
         <Filter placeholder="User Name" fieldName="User Name" />
       </ControlPanel>
 
-      <SC.TableWrapper>
-        <CustomersDataTable customers={[]} />
-      </SC.TableWrapper>
+      {customers.length > 0 && (
+        <SC.TableWrapper>
+          <CustomersDataTable customers={customers} />
+        </SC.TableWrapper>
+      )}
     </PageWrapper>
   );
 };
