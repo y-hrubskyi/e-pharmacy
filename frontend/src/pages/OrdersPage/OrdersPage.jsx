f1ff1da1923b8310571d111e0520a2fd8d1949de
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-24 09:13:13 +0300

    feat: add simple fetching orders data

--- frontend/src/pages/OrdersPage/OrdersPage.jsx
+++ frontend/src/pages/OrdersPage/OrdersPage.jsx
@@ -1,18 +1,39 @@
+import { useEffect, useState } from "react";
+
+import API from "#services/axios";
+
 import { PageWrapper, ControlPanel } from "#components/common/Page/Page.styled";
 import { Filter } from "#components/common/Filter/Filter";
 import { TableWrapper } from "#components/common/Table/Table.styled";
 import { AllOrdersTable } from "#components/AllOrdersTable/AllOrdersTable";
 
 const OrdersPage = () => {
+  const [orders, setOrders] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await API.get("/orders");
+        setOrders(data);
+      } catch (error) {
+        //
+      }
+    })();
+  }, []);
+
   return (
     <PageWrapper>
       <ControlPanel>
         <Filter placeholder="User Name" fieldName="User Name" />
       </ControlPanel>
 
-      <TableWrapper>
-        <AllOrdersTable orders={[]} />
-      </TableWrapper>
+      {orders?.paginatedResult?.length && (
+        <>
+          <TableWrapper>
+            <AllOrdersTable orders={orders.paginatedResult} />
+          </TableWrapper>
+        </>
+      )}
     </PageWrapper>
   );
 };
