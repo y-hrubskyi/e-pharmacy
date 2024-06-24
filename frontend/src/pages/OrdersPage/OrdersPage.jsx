dcad853aba8383ab45753cbf38ff011eb16f4906
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-24 09:15:47 +0300

    feat: add data fetching states for OrdersPage

--- frontend/src/pages/OrdersPage/OrdersPage.jsx
+++ frontend/src/pages/OrdersPage/OrdersPage.jsx
@@ -2,24 +2,42 @@ import { useEffect, useState } from "react";
 
 import API from "#services/axios";
 
-import { PageWrapper, ControlPanel } from "#components/common/Page/Page.styled";
+import {
+  PageWrapper,
+  ControlPanel,
+  PlaceholderWrapper,
+} from "#components/common/Page/Page.styled";
 import { Filter } from "#components/common/Filter/Filter";
 import { TableWrapper } from "#components/common/Table/Table.styled";
 import { AllOrdersTable } from "#components/AllOrdersTable/AllOrdersTable";
+import { Loader } from "#components/common/Loader/Loader";
+import { Placeholder } from "#components/common/Placeholder/Placeholder";
 
 const OrdersPage = () => {
   const [orders, setOrders] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     (async () => {
       try {
+        setIsLoading(true);
+        setError(null);
+
         const { data } = await API.get("/orders");
         setOrders(data);
       } catch (error) {
-        //
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     })();
   }, []);
+
+  const loading = !error && isLoading;
+  const hasError = !isLoading && error;
+  const content = !error && orders?.paginatedResult?.length > 0;
+  const noData = !isLoading && !error && orders?.paginatedResult?.length === 0;
 
   return (
     <PageWrapper>
@@ -27,12 +45,25 @@ const OrdersPage = () => {
         <Filter placeholder="User Name" fieldName="User Name" />
       </ControlPanel>
 
-      {orders?.paginatedResult?.length && (
+      {content && (
         <>
           <TableWrapper>
             <AllOrdersTable orders={orders.paginatedResult} />
           </TableWrapper>
         </>
+      )}
+      {loading && <Loader />}
+      {hasError && (
+        <PlaceholderWrapper>
+          <Placeholder>Oops... {error}</Placeholder>
+        </PlaceholderWrapper>
+      )}
+      {noData && (
+        <PlaceholderWrapper>
+          <Placeholder>
+            Nothing found. Please check your spelling or try other terms
+          </Placeholder>
+        </PlaceholderWrapper>
       )}
     </PageWrapper>
   );
