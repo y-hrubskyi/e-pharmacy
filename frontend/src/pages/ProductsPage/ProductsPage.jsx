8ae94450cf2cd88d618fa7c5f7e90483442ed868
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-24 15:03:11 +0300

    feat: add to OrdersPage created earlier components
     - Filter
     - AddBtnWithPlusIcon
     - AllProductsTable

--- frontend/src/pages/ProductsPage/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage/ProductsPage.jsx
@@ -1,5 +1,24 @@
+import { PageWrapper, ControlPanel } from "#components/common/Page/Page.styled";
+import { Filter } from "#components/common/Filter/Filter";
+import { AddBtnWithPlusIcon } from "#components/common/AddBtnWithPlusIcon/AddBtnWithPlusIcon";
+import { TableWrapper } from "#components/common/Table/Table.styled";
+import { AllProductsTable } from "#components/AllProductsTable/AllProductsTable";
+
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <PageWrapper>
+      <ControlPanel>
+        <Filter placeholder="Product Name" fieldName="Product Name" />
+        <AddBtnWithPlusIcon onClick={() => {}}>
+          Add a new product
+        </AddBtnWithPlusIcon>
+      </ControlPanel>
+
+      <TableWrapper>
+        <AllProductsTable products={[]} />
+      </TableWrapper>
+    </PageWrapper>
+  );
 };
 
 export default ProductsPage;
