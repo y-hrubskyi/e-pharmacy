fbc3818ff754d31559bfd52a91f38c65faf4c764
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-25 15:01:53 +0300

    feat: add pagination functionality for SuppliersPage

--- frontend/src/pages/SuppliersPage/SuppliersPage.jsx
+++ frontend/src/pages/SuppliersPage/SuppliersPage.jsx
@@ -11,12 +11,14 @@ import { Filter } from "#components/common/Filter/Filter";
 import { AddBtnWithoutPlusIcon } from "#components/common/AddBtnWithoutPlusIcon/AddBtnWithoutPlusIcon.styled";
 import { TableWrapper } from "#components/common/Table/Table.styled";
 import { AllSuppliersTable } from "#components/AllSuppliersTable/AllSuppliersTable";
+import { Paginator } from "#components/common/Paginator/Paginator";
 import { Loader } from "#components/common/Loader/Loader";
 import { Placeholder } from "#components/common/Placeholder/Placeholder";
 
 const SuppliersPage = () => {
   const [suppliers, setSuppliers] = useState(null);
   const [filter, setFilter] = useState("");
+  const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -26,7 +28,7 @@ const SuppliersPage = () => {
         setIsLoading(true);
         setError(null);
 
-        const searchParams = new URLSearchParams();
+        const searchParams = new URLSearchParams({ page });
         if (filter) searchParams.set("name", filter.split("/")[0]);
 
         const { data } = await API.get(`/suppliers?${searchParams}`);
@@ -37,10 +39,11 @@ const SuppliersPage = () => {
         setIsLoading(false);
       }
     })();
-  }, [filter]);
+  }, [page, filter]);
 
   const onFilterSubmit = (value) => {
     setFilter(`${value}/${Date.now()}`);
+    setPage(1);
     setSuppliers(null);
   };
 
@@ -65,9 +68,12 @@ const SuppliersPage = () => {
       </ControlPanel>
 
       {content && (
-        <TableWrapper>
-          <AllSuppliersTable suppliers={suppliers.paginatedResult} />
-        </TableWrapper>
+        <>
+          <TableWrapper>
+            <AllSuppliersTable suppliers={suppliers.paginatedResult} />
+          </TableWrapper>
+          <Paginator totalCount={suppliers.totalCount} setPage={setPage} />
+        </>
       )}
       {loading && <Loader />}
       {hasError && (
