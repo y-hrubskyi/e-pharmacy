1a71ce3d5980cd70846f859c962c611e2f4fc3e1
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-25 14:58:35 +0300

    feat: add data fetching states for SuppliersPage

--- frontend/src/pages/SuppliersPage/SuppliersPage.jsx
+++ frontend/src/pages/SuppliersPage/SuppliersPage.jsx
@@ -2,25 +2,45 @@ import { useEffect, useState } from "react";
 
 import API from "#services/axios";
 
-import { PageWrapper, ControlPanel } from "#components/common/Page/Page.styled";
+import {
+  PageWrapper,
+  ControlPanel,
+  PlaceholderWrapper,
+} from "#components/common/Page/Page.styled";
 import { Filter } from "#components/common/Filter/Filter";
 import { AddBtnWithoutPlusIcon } from "#components/common/AddBtnWithoutPlusIcon/AddBtnWithoutPlusIcon.styled";
 import { TableWrapper } from "#components/common/Table/Table.styled";
 import { AllSuppliersTable } from "#components/AllSuppliersTable/AllSuppliersTable";
+import { Loader } from "#components/common/Loader/Loader";
+import { Placeholder } from "#components/common/Placeholder/Placeholder";
 
 const SuppliersPage = () => {
   const [suppliers, setSuppliers] = useState(null);
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     (async () => {
       try {
+        setIsLoading(true);
+        setError(null);
+
         const { data } = await API.get("/suppliers");
         setSuppliers(data);
       } catch (error) {
-        //
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     })();
   }, []);
+
+  const loading = !error && isLoading;
+  const hasError = !isLoading && error;
+  const content = !error && suppliers?.paginatedResult?.length > 0;
+  const noData =
+    !isLoading && !error && suppliers?.paginatedResult?.length === 0;
 
   return (
     <PageWrapper>
@@ -31,13 +51,23 @@ const SuppliersPage = () => {
         </AddBtnWithoutPlusIcon>
       </ControlPanel>
 
-      {suppliers?.paginatedResult?.length > 0 && (
+      {content && (
         <TableWrapper>
-          <AllSuppliersTable
-            suppliers={suppliers.paginatedResult}
-            setProducts={setSuppliers}
-          />
+          <AllSuppliersTable suppliers={suppliers.paginatedResult} />
         </TableWrapper>
+      )}
+      {loading && <Loader />}
+      {hasError && (
+        <PlaceholderWrapper>
+          <Placeholder>Oops... {error}</Placeholder>
+        </PlaceholderWrapper>
+      )}
+      {noData && (
+        <PlaceholderWrapper>
+          <Placeholder>
+            Nothing found. Please check your spelling or try other terms
+          </Placeholder>
+        </PlaceholderWrapper>
       )}
     </PageWrapper>
   );
