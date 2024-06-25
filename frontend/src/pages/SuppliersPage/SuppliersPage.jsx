46371b60c5973dbff9bd9de6a901156e0d6231d6
y-hrubskyi <y.hrubskyi@gmail.com>
2024-06-25 14:52:36 +0300

    feat: add to SuppliersPage created earlier components
     - Filter
     - AddBtnWithoutPlusIcon
     - AllSuppliersTable

--- frontend/src/pages/SuppliersPage/SuppliersPage.jsx
+++ frontend/src/pages/SuppliersPage/SuppliersPage.jsx
@@ -1,5 +1,24 @@
+import { PageWrapper, ControlPanel } from "#components/common/Page/Page.styled";
+import { Filter } from "#components/common/Filter/Filter";
+import { AddBtnWithoutPlusIcon } from "#components/common/AddBtnWithoutPlusIcon/AddBtnWithoutPlusIcon.styled";
+import { TableWrapper } from "#components/common/Table/Table.styled";
+import { AllSuppliersTable } from "#components/AllSuppliersTable/AllSuppliersTable";
+
 const SuppliersPage = () => {
-  return <div>SuppliersPage</div>;
+  return (
+    <PageWrapper>
+      <ControlPanel>
+        <Filter placeholder="Supplier Name" fieldName="Supplier Name" />
+        <AddBtnWithoutPlusIcon type="button" onClick={() => {}}>
+          Add a new supplier
+        </AddBtnWithoutPlusIcon>
+      </ControlPanel>
+
+      <TableWrapper>
+        <AllSuppliersTable suppliers={[]} />
+      </TableWrapper>
+    </PageWrapper>
+  );
 };
 
 export default SuppliersPage;
